81ef28415455ec4ed6916e490a2ae50b37631744
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2026-04-14 21:10:29 +0200

    add excel export

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Run this file to run the model and save the results"""
+import os
 import plotly.express as px
 
 from heat_model import \
@@ -7,30 +8,37 @@ from heat_model import \
     calculate_heat_pump_power
 from read_data import create_dataframe_from_input
 
+A = 400  # m²; surface area
+# I'm assuming a cubic house with one shared wall, for 80m² a side. Windows/doors take up 25% of wall area.
+# U-values are taken from https://publications.jrc.ec.europa.eu/repository/bitstream/JRC117739/cost_optimal_energy_renovations_online.pdf
+# as the averages of U-values listed in Figure 2 for the Netherlands
+U = (80*1.1 + .75*160*1.45 + .25*160*1.3 + 80*2.25)/A  # W/m²K; average heat transfer coefficient
+
+THERMOSTAT = {
+# time: temp (°C)
+    7: 19,
+    21: 16
+}
+
+# heat pump COP model: efficiency * t_cond / (t_cond - t_outside)
+HEAT_PUMP_EFFICIENCY = .6
+HEAT_PUMP_T_COND = 35  # °C
 
 if __name__ == "__main__":
     DATA_FILE = "data/1997.txt"
-    A = 400  # m²; surface area
-    # I'm assuming a cubic house with one shared wall, for 80m² a side. Windows/doors take up 25% of wall area.
-    # U-values are taken from https://publications.jrc.ec.europa.eu/repository/bitstream/JRC117739/cost_optimal_energy_renovations_online.pdf
-    # as the averages of U-values listed in Figure 2 for the Netherlands
-    U = (80*1.1 + .75*160*1.45 + .25*160*1.3 + 80*2.25)/A  # W/m²K; average heat transfer coefficient
 
-    THERMOSTAT = {
-    # time: temp (°C)
-        7: 19,
-        21: 16
-    }
-    HEAT_PUMP_EFFICIENCY = .6
-    HEAT_PUMP_T_COND = 35  # °C
-
+    # create a dataframe from the data, then create a thermostat setpoint array and calculate the heat demand and heat pump power
     data = create_dataframe_from_input(data_file=DATA_FILE)
     data["t_set"] = create_daily_temperature_setpoint(THERMOSTAT, repeat=365).values
     data["Q"] = calculate_heat_demand_simple(u=U,a=A, t_set=data["t_set"], t_outside=data["temp"]).values
     data["heat_pump"] = calculate_heat_pump_power(HEAT_PUMP_EFFICIENCY, HEAT_PUMP_T_COND, data["temp"], data["Q"])
 
-    print(data["heat_pump"][data["heat_pump"]>0].median())
+    # save data to excel
+    if not os.path.exists("output"):
+        os.mkdir("output")
+    data.to_excel("output/result.xlsx")
 
+    # create figures
     temp_fig = px.line(
         data,
         x="date",
