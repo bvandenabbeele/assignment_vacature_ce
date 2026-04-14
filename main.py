46b704e3be487bbaf99539142ea69617125aa646
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2026-04-14 20:45:54 +0200

    add heat pump power and readd plots

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 """Run this file to run the model and save the results"""
+import plotly.express as px
 
-from heat_model import create_daily_temperature_setpoint, calculate_heat_demand_simple
-from plots import create_temp_fig, create_q_fig
+from heat_model import \
+    create_daily_temperature_setpoint, \
+    calculate_heat_demand_simple, \
+    calculate_heat_pump_power
 from read_data import create_dataframe_from_input
 
 
@@ -18,10 +21,41 @@ if __name__ == "__main__":
         7: 19,
         21: 16
     }
+    HEAT_PUMP_EFFICIENCY = .6
+    HEAT_PUMP_T_COND = 35  # °C
 
     data = create_dataframe_from_input(data_file=DATA_FILE)
     data["t_set"] = create_daily_temperature_setpoint(THERMOSTAT, repeat=365).values
     data["Q"] = calculate_heat_demand_simple(u=U,a=A, t_set=data["t_set"], t_outside=data["temp"]).values
+    data["heat_pump"] = calculate_heat_pump_power(HEAT_PUMP_EFFICIENCY, HEAT_PUMP_T_COND, data["temp"], data["Q"])
 
-    create_temp_fig(data).write_image("figures/temperature.png")
-    create_q_fig(data, THERMOSTAT).write_image("figures/heat_demand.png")
+    print(data["heat_pump"][data["heat_pump"]>0].median())
+
+    temp_fig = px.line(
+        data,
+        x="date",
+        y="temp",
+        labels={"date": "Date", "temp": "Temperature (°C)"},
+        title=f"Temperature recorded between {min(data['date']).strftime('%d %b %Y')} and {max(data['date']).strftime('%d %b %Y')} in De Bilt"
+    )
+    temp_fig.write_image("figures/temperature.png")
+
+    thermo_info = f"based on thermostat settings: {', '.join([f'{temp}°C from {time}:00' for time, temp in THERMOSTAT.items()])}"
+
+    q_fig = px.line(
+        data,
+        x="date",
+        y="Q",
+        labels={"date": "Date", "Q": "Heat Demand (kWh/h)"},
+        title=f"Heat demand {thermo_info}"
+    )
+    q_fig.write_image("figures/heat_demand.png")
+
+    power_fig = px.line(
+        data,
+        x="date",
+        y="heat_pump",
+        labels={"date": "Date", "heat_pump": "Heat Pump Power (kW)"},
+        title=f"Heat Pump Power {thermo_info}"
+    )
+    power_fig.write_image("figures/power.png")
